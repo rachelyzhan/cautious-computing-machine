cc6cc78453a852e8ea621d0df6faacaf610b3046
Rachel Zhan <rachel.zhan@masonohioschools.com>
2017-06-26 22:29:50 +0000

    Add key binfing to that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -5,3 +5,9 @@ function sayThatWasEasy() {
 
 $("#easy").on("click", sayThatWasEasy);
 
+$(document).keypress(delegateKeypress);
+function delegateKeypress(event){
+    if (event.charCode == 32) {
+        $("#easy").trigger("click");
+    }
+}
